67a4797675e06457f26d53166db4378c7d61fc8f
Chris W <wells457@umn.edu>
2020-03-10 23:56:41 -0500

    I tried to do the local storage with the highscores, but couldn't quite get it to work. I also made a README and added comments both of what's happening, and what should be happenign

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Quiz
+
+
+The goal of this was to create a quiz which 
+had a timer that counted down to 0
+had the quiz end when the timer got to 0
+had the quiz end when all the questions were answered
+told the user if they answered the question correctly or incorrectly
+applied a penalty to the time if they clicked the wrong answer
+and gave the total score at the end.
+
+While the current quiz is not very aesthetically pleasing, it does do those things.
+
+It was also expected that
+the user could type in their initals at the end
+the intitals along side the score would appea on a high score list
+these values would be saved to the local storage.
+
+This only partially works. I ran into trouble getting all of this to run correctly, but my code has some comments describing what I believe should happen
--- script.js
+++ script.js
@@ -7,222 +7,253 @@
 // have a timer showing which is penalized for every wrong answer
 
 
+// Declare all of our questions in one array for convinence
 
-var Question1="What does HTML stand for?";
-var Answer=["Right", "Wrong", "Wrong", "Wrong"]
-var allQuestions=["What does HTML stand for?", "What does CSS stand for?", "What is the relationship bewteen java and javascript?", "Which best describes bootstrap is terms of coding?", "Which is a self-closing tag?" ]
+var allQuestions = ["What does HTML stand for?", "What does CSS stand for?", "What is the relationship bewteen java and javascript?", "Which best describes bootstrap is terms of coding?", "Which is a self-closing tag?"]
+
+// Declare 5 Objects which will hold questions and answers
 
 var Q1 = {
 
     question: allQuestions[0],
     answers: ["Hypertext Markup Language", "High Texture Machine Learning", "Hot Type Marker Level", "Hairy Tailors Make Leggings"]
- 
+
 }
 
 var Q2 = {
 
     question: allQuestions[1],
     answers: ["Cascading Style Sheet", "Computer Synchomatic Sentence", "Cyber System Syllable", "Crazy Shocking Soup"]
- 
+
 }
 var Q3 = {
 
     question: allQuestions[2],
-    answers: ["Javascript mooched off of Java's popularity but is completly different", "They are the same", "They had the same creator","Javascript is java with extra features" ]
- 
+    answers: ["Javascript mooched off of Java's popularity but is completly different", "They are the same", "They had the same creator", "Javascript is java with extra features"]
+
 }
 var Q4 = {
 
     question: allQuestions[3],
     answers: ["A CSS Library", "A method of extrapolating data", "A piece of fabric used for footwear", "Bill Turner's nickname"]
- 
+
 }
 var Q5 = {
 
     question: allQuestions[4],
     answers: ["<img>", "<div>", "<a>", "<p>"]
- 
+
 }
 
 
 console.log(Q1)
 
 
-// This function dynamically creates text for the questions and answers whatever object is fed to it
-// function ask(object){
-
-// $("#Q").text(object.question)
-// console.log(object.question)
-// $("#A").text(object.answers[0])
-// $("#B").text(object.answers[1])
-// $("#C").text(object.answers[2])
-// $("#D").text(object.answers[3])
-// }
+// Declare some more variables
 
 var object;
-var Qs=[Q1,Q2,Q3,Q4,Q5];
-var counter=0;
-var time=3000;
+var Qs = [Q1, Q2, Q3, Q4, Q5];
+var counter = -1;
+var time = 3000;
 var current;
 var penalty;
-var secondsLeft=30;
-var score=0;
+var secondsLeft = 30;
+var score = 0;
 
 // Dynamic Functions
 
+// This functions starts the timer
 function setTime() {
-   
-    timerInterval = setInterval(function() {
-      // this is a minus minus, removing one each interval
-      secondsLeft--;
-      current=secondsLeft;
-      $("#timer").text(secondsLeft)
-  
-      if(secondsLeft < 1) {
-        // stops the time
-        endScreen();
-      }
-  
-      // this is 1000 milleseconds
+
+
+    timerInterval = setInterval(function () {
+        // this is a minus minus, removing one second each interval
+        secondsLeft--;
+        current = secondsLeft;
+        //   this dynamically puts the number of seconds left on the index page
+        $("#timer").text(secondsLeft)
+        // when they timer hits 0 (or under due to penalties) it calls the endScreen() function
+        if (secondsLeft < 1) {
+            // stops the time
+            endScreen();
+        }
+
+        // this is 1000 milleseconds, so there's one second between each interval
     }, 1000);
-  }
+}
 
-
-function penalize(){
-    secondsLeft=secondsLeft-5;
-    console.log("penal")
+// This is the penalize() funciton. It is called whenever a wrong answer is clicked
+function penalize() {
+    // 5 seconds are removed from the timer
+    secondsLeft = secondsLeft - 5;
 
 
 }
 
-
-function endScreen(){
+// This is called when all questions have been answered or time runs out
+function endScreen() {
 
     $("#wrong").append("        Your final score is ", score, " out of 5")
     clearInterval(timerInterval);
 
 }
 
-// function postInitials(){
-var endForm=document.querySelector("#end-form")
-var endInput=document.querySelector("#end-text")
+// These variables are declared, and used for the initials and high scores
+var endForm = document.querySelector("#end-form")
+var endInput = document.querySelector("#end-text")
 var endList = document.querySelector("#end-list");
 
-// make initials bar appear
 
-// When form is submitted...
-// endForm.addEventListener("submit", function(event) {
-//     event.preventDefault();
-  
-//     var endText = endInput.value.trim();
+var initial
+var initials = []
+var totalScores = []
+var highScores = []
+var highScoreNumbers = []
 
-//       // Return from function early if submitted initials are blank
-//   if (endText === "") {
-//     return;
-//   }
 
-//   var initials=[]
-//   initials.push(endText)
+// This event listener is supposed to allow a user to enetr their initials, 
+// then store those initials in an array in the local storage, then print out the new intitial and score
 
-// })
-
-var initials=[]
-endForm.addEventListener("submit", function(event){
+endForm.addEventListener("submit", function (event) {
     event.preventDefault();
     var endText = endInput.value
 
-      // Return from function early if submitted initials are blank
-  if (endText === "") {
-    return;
-  }
 
-  
-  initials.push(endText)
-console.log("initials array "+ initials)
+    // Return from function early if submitted initials are blank
+    if (endText === "") {
+        return;
+    }
 
 
-endList.innerHTML = "";
-  // Render a new li for each set of initials
-  for (var i = 0; i < initials.length; i++) {
-    var initial = initials[i];
+    initials.push(endText)
+    totalScores.push(score)
 
-    var li = document.createElement("li");
-    li.textContent = (initial + "   "+ score);
-    endList.appendChild(li);
-  }
+    //   put the new initials and scores on the local storage
+    localStorage.setItem("Initials", initials);
+    localStorage.setItem("SCORES", totalScores)
+
+    console.log("initials array " + initials)
+
+    // pull the ew initials and score down from local storage
+    highScores.push(localStorage.getItem("Initials"))
+    highScoreNumbers.push(localStorage.getItem("SCORES"))
 
 
+    console.log("high score " + highScores)
+    endList.innerHTML = "";
+    // Render a new li for each set of initials
+    for (var i = 0; i < highScores.length; i++) {
 
+        var li = document.createElement("li");
+
+        li.textContent = (highScores[i] + "   " + highScoreNumbers[i]);
+        endList.appendChild(li);
+    }
+
+
+    // end of initials event listener
 })
 
-// }
-// postInitials();
+// This function is supposed to get all of the initals and scores down from the local storage and print them out
+function ListHighScores() {
+    // get all of the initials from the local storage
+    highScores.push(localStorage.getItem("Initials"))
+    // get all of the scores from the local storage
+    highScoreNumbers.push(localStorage.getItem("SCORES"))
+
+    console.log("auto " + highScores)
+    for (var i = 0; i < highScores.length; i++) {
+
+        var li = document.createElement("li");
+        li.textContent = (highScores[i] + "   " + highScoreNumbers[i]);
+        endList.appendChild(li);
+    }
+
+}
+// This calls the above function. In theory, the amove function will run as soon as the browser opens, 
+// The current high scores will be printed, and when a new high score is submitted, the previous event listener would add it to the list
+ListHighScores();
 
 
-// Answer Functions
+
+
+// Answer Functions: These are the functions reliant on clicking the buttons
+// Currently, all of the correct ansers are for button A
 
 // A (The correct answer) function
- function answerA(){
-  console.log("A Click")
-counter++
-        
-        console.log("COUNTER " +counter) 
+function answerA() {
+    // This makes it so clicking the A button before the Start button does nothing
+    if (counter === -1) {
+        return;
+    }
+    // This makes it so cliking the A button after the quiz is over does nothing
+    if (counter > 5) {
 
-        if (counter === 5){
-            // show end screen
-            endScreen();
-            return;
-        }
-        if (counter > 5){
+        return;
+    }
 
-            return;
-        }
-        $("#wrong").text("Correct!")
+    score++
+    console.log("A Click")
+    counter++
 
-        score++
-        object=Qs[counter]
+    console.log("COUNTER " + counter)
+    // this runs the endScreen function when the quiz ends
+    if (counter === 5) {
+        // show end screen
+        endScreen();
+        return;
+    }
+    // This prints the word "Correct"
+    $("#wrong").text("Correct!")
 
-        $("#Q").text(object.question)
-        console.log("pls "+object.question)
-        console.log(object.answers[0])
-        $("#A").text(object.answers[0])
-        $("#B").text(object.answers[1])
-        $("#C").text(object.answers[2])
-        $("#D").text(object.answers[3])
-        
-        }
-    
+
+    object = Qs[counter]
+
+    $("#Q").text(object.question)
+    console.log("pls " + object.question)
+    console.log(object.answers[0])
+    $("#A").text(object.answers[0])
+    $("#B").text(object.answers[1])
+    $("#C").text(object.answers[2])
+    $("#D").text(object.answers[3])
+
+}
+
 
 // B (the wrong answer) function
-function AnswerB(){
-object=Qs[counter]
-counter++
+function AnswerB() {
+    if (counter === -1) {
+        return;
+    }
+    object = Qs[counter]
+    counter++
 
 
 
 
-if (counter === 5){
-    // show end screen
-    endScreen();
+    if (counter === 5) {
+        // show end screen
+        endScreen();
 
-    return;
-}
+        return;
+    }
 
-if (counter > 5){
+    if (counter > 5) {
 
-    return;
-}
-$("#wrong").text("Wrong!")
+        return;
+    }
+    $("#wrong").text("Wrong!")
 
-object=Qs[counter]
-penalize()
-$("#Q").text(object.question)
-console.log(object.question)
-console.log(object.answers[0])
-$("#A").text(object.answers[0])
-$("#B").text(object.answers[1])
-$("#C").text(object.answers[2])
-$("#D").text(object.answers[3])
+    object = Qs[counter]
+    // this calls the penalize function because you clicked the wrong answer
+    penalize()
+    $("#Q").text(object.question)
+    console.log(object.question)
+    console.log(object.answers[0])
+    $("#A").text(object.answers[0])
+    $("#B").text(object.answers[1])
+    $("#C").text(object.answers[2])
+    $("#D").text(object.answers[3])
 
 }
 
@@ -231,14 +262,20 @@ $("#D").text(object.answers[3])
 
 
 // START event listener
-$("#start").on("click", function (){
-    object=Qs[counter]
-    
-  setTime();
-    if (counter > 4){
-        // show end screen
+// This starts the quiz by clicking the start button, but does nothing after that
+$("#start").on("click", function () {
+
+
+
+    if (counter > -1) {
         return;
     }
+
+    counter++
+
+    object = Qs[counter]
+    //   This calls the timer function to set the timer
+    setTime();
 
     $("#Q").text(object.question)
     console.log(object.question)
@@ -247,104 +284,14 @@ $("#start").on("click", function (){
     $("#B").text(object.answers[1])
     $("#C").text(object.answers[2])
     $("#D").text(object.answers[3])
-    }
+}
 )
 
 // Event listeners for each button
+// Currently, clicking the A button will always lead to the correct answer
+// clicking any other button leads to a wrong answer
 $("#A").on("click", answerA)
 $("#B").on("click", AnswerB)
 $("#C").on("click", AnswerB)
 $("#D").on("click", AnswerB)
 
-  
-//     object=Qs[counter]
-//     counter++
-//     $("#wrong").text("Correct!")
-//     if (counter > 4){
-//         // show end screen
-//         return;
-//     }
-
-//     $("#Q").text(object.question)
-//     console.log(object.question)
-//     console.log(object.answers[0])
-//     $("#A").text(object.answers[0])
-//     $("#B").text(object.answers[1])
-//     $("#C").text(object.answers[2])
-//     $("#D").text(object.answers[3])
-//     }
-// )
-
-// $("#B").on("click", AnswerB)
-
-
-// $("#B").on("click", function (){
-    // object=Qs[counter]
-    // counter++
-    // penalize()
-
-    // $("#wrong").text("Wrong!")
-
-    // if (counter > 4){
-    //     // show end screen
-    //     return;
-    // }
-
-    // $("#Q").text(object.question)
-    // console.log(object.question)
-    // console.log(object.answers[0])
-    // $("#A").text(object.answers[0])
-    // $("#B").text(object.answers[1])
-    // $("#C").text(object.answers[2])
-    // $("#D").text(object.answers[3])
-    // }
-// )
-
-// $("#C").on("click", function (){
-//     object=Qs[counter]
-//     counter++
-//     // setTime(penalty)
-
-//     $("#wrong").text("Wrong!")
-
-//     if (counter > 4){
-//         // show end screen
-//         return;
-//     }
-
-//     $("#Q").text(object.question)
-//     console.log(object.question)
-//     console.log(object.answers[0])
-//     $("#A").text(object.answers[0])
-//     $("#B").text(object.answers[1])
-//     $("#C").text(object.answers[2])
-//     $("#D").text(object.answers[3])
-//     }
-// )
-// $("#D").on("click", function (){
-//     object=Qs[counter]
-//         counter++   
-     
-//         $("#wrong").text("Wrong!")
-//     if (counter > 4){
-//         // show end screen
-//         return;
-//     }
-
-//     $("#Q").text(object.question)
-//     console.log(object.question)
-//     console.log(object.answers[0])
-//     $("#A").text(object.answers[0])
-//     $("#B").text(object.answers[1])
-//     $("#C").text(object.answers[2])
-//     $("#D").text(object.answers[3])
-//     }
-// )
-
-
-
-
-// $("#A").on("click", ask(Q2))
-// $("#A").on("click", ask(Q3))
-// $("#A").on("click", ask(Q4))
-// $("#A").on("click", ask(Q5)  
